9202bd0c70f7202d274e4227707eebbc035c2dea
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2023-12-05 17:37:12 -0400

    puesta de password

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,79 @@ import frase_data from './data/frases'
 
 function App() {
   const [frase, setFrase] = useState(frase_data[Math.floor(Math.random() * frase_data.length)])
+  const [isError, setIsError] = useState(true);
+  const [password, setPassword] = useState('');
 
-  console.log("frase", frase)
+  if (password === '081219JJ') {
+    setIsError(false)
+    setPassword('')
+  }
+
+  // console.log("isError", isError)
+
+  // console.log("frase", frase)
   return (
     <>
-      <h1>Jhesy</h1>
-      <br />
-      <p className='f-perdon'>Se que mis errores , te han causado inseguridades, se que cada lagrima
-        <br />que has derramado no la merezco, y cada una de ellas sera mi tortura
-        <br />pero de los miles de errores que cometere en mi vida
-        <br />el perderte no es una opcion..
-      </p>
-      <div>
-        <a target="_blank">
-          <img src={ mor } className="foto-jhes" alt="Vite logo" />
-        </a>
-        <a target="_blank">
-          <img src={ mor2 } className="foto-jhes" alt="Vite logo" />
-        </a>
-        <a target="_blank">
-          <img src={ mor3 } className="foto-jhes" alt="Vite logo" />
-        </a>
+      {
+        isError ?
+          <>
+            <form onSubmit={ ev => {
+              ev.preventDefault();
+              // console.log("ev", ev.target.password.value)
+            } }>
+              <input
+                type="password"
+                id="password"
+                name='password'
+                placeholder="pronto ........ "
+                onChange={ ev => {
+                  setPassword(ev.target.value)
+                } }
+              />
+              <button type='submit'
+              >probar</button>
+            </form>
+          </> :
+          <>
+            <h1>Jhesy</h1>
+            <br />
+            <p className='f-perdon'>
+              Aveces la vida nos cruza con personas pasajeras, pero dime porfavor que no eres una de ellas
+              <br />
+              Por que dejaria mi camino , para estar contigo
+            </p>
 
-      </div>
-      <h1>Mi Vida</h1>
-      <div className="card">
-        <button onClick={
-          () => {
-            setFrase(frase_data[Math.floor(Math.random() * frase_data.length)])
-          }
-        } >
-          TOCAME
-        </button>
-      </div>
-      <div className="container fa fa-heart">
-        <div className="contenido">
-          <h1>para ti ..................... : </h1>
-          <p>{ frase.frase }
-          </p>
-        </div>
-      </div>
+            <div>
+              <a target="_blank">
+                <img src={ mor } className="foto-jhes" alt="Vite logo" />
+              </a>
+              <a target="_blank">
+                <img src={ mor2 } className="foto-jhes" alt="Vite logo" />
+              </a>
+              <a target="_blank">
+                <img src={ mor3 } className="foto-jhes" alt="Vite logo" />
+              </a>
 
+            </div>
+            <h1>Mi Vida</h1>
+            <div className="card">
+              <button onClick={
+                () => {
+                  setFrase(frase_data[Math.floor(Math.random() * frase_data.length)])
+                }
+              } >
+                TOCAME
+              </button>
+            </div>
+            <div className="container fa fa-heart">
+              <div className="contenido">
+                <h1>para ti ..................... : </h1>
+                <p>{ frase.frase }
+                </p>
+              </div>
+            </div>
+          </>
+      }
     </>
   )
 }
